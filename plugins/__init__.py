f6a5a316ce8c1d3fb6468b615a9b2d88eb494c4a
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-07-24 13:12:28 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,28 +1,13 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
-
-
 from aiohttp import web
 from .route import routes
 
+routes = web.RouteTableDef()
 
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("MATRIX")
+  
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
