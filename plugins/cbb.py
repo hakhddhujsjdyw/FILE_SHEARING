dd7605bae62e95adb9622f786a1b5f73e73e7b08
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-14 19:59:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>𝐎𝐍𝐋𝐘 𝐅𝐎𝐑 𝐖𝐎𝐑𝐊𝐈𝐍𝐆 𝐀𝐂 𝐌𝐎𝐕𝐈𝐄𝐒 𝐁𝐎𝐓 🤗</b>\n\n️<b>📌 𝗔𝗻𝘆 𝗛𝗲𝗹𝗽 𝗣𝗹𝗲𝗮𝘀𝗲 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 𝗔𝗱𝗺𝗶𝗻 : @ARAKAL_THERAVAD_MOVIES_02_bot</b>",
+            text = f"<b>𝐎𝐍𝐋𝐘 𝐅𝐎𝐑 𝐖𝐎𝐑𝐊𝐈𝐍𝐆 MCU 𝐌𝐎𝐕𝐈𝐄𝐒 𝐁𝐎𝐓 🤗</b>\n\n️<b>📌 𝗔𝗻𝘆 𝗛𝗲𝗹𝗽 𝗣𝗹𝗲𝗮𝘀𝗲 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 𝗔𝗱𝗺𝗶𝗻 : @MCU_ADMIN_V1_BOT</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
-                    [
-                        InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
+                    [                        
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
