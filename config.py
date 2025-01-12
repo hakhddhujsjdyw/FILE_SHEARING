80d288f4477a60961164916e842fe60f78afa631
hakhddhujsjdyw <manafalthaf01@gmail.com>
2025-01-12 15:33:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,21 +8,21 @@ from logging.handlers import RotatingFileHandler
 
 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7641553821:AAHiYKc6BfTamdHJu9ybKkBor33JF-uyR2o")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", "19071424"))
 API_HASH = os.environ.get("API_HASH", "c4b3e298cc50fd4cc563ae75ee882948")
 
 
 OWNER = os.environ.get("OWNER", "7425490417") #Owner username
 OWNER_ID = int(os.environ.get("OWNER_ID", "7425490417")) #Owner user id
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Autofilterv7:Autofilterv7@cluster0.t5tqe4s.mongodb.net/")
-DB_NAME = os.environ.get("DB_NAME", "Autofilterv7")
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "")
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/ee0cdd28fdd53d3df26c7.jpg https://telegra.ph/file/def62cc4e67aabc03d4c8.jpg https://telegra.ph/file/0c04c8e0b4a67d1716f53.jpg https://telegra.ph/file/eccd0bd3f18111afb9726.jpg https://telegra.ph/file/d6a99238948a8918e3e94.jpg https://telegra.ph/file/fcaf8e6dbadb19364ab54.jpg https://telegra.ph/file/6ca5d5dc24e029c4fe642.jpg https://telegra.ph/file/80a417b7cfc6dc262dcee.jpg https://telegra.ph/file/0426befe276a430a6cdf0.jpg https://telegra.ph/file/7d716533a70fad271a5b9.jpg')).split()
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001974433785"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002247041357"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002320550967"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
@@ -48,7 +48,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {first}\n\n♦️ READ
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{previouscaption}\n\n╔═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╗\n➲ @MCUupdatesLINKS\n╚═══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╝</b>")
 
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "False" else False
 
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
